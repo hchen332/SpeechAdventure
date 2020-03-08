8860b3885c962da8188d94bb91b87aed34f7d721
hchen332 <56405172+hchen332@users.noreply.github.com>
2020-03-08 04:21:03 -0700

    Update main.js

--- 04-speech/src/main/webapp/javascript/main.js
+++ 04-speech/src/main/webapp/javascript/main.js
@@ -12,56 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function helperDrawingFunctions() {
-  CanvasRenderingContext2D.prototype.line = function(x1, y1, x2, y2) {
-    this.lineCap = 'round';
-    this.beginPath();
-    this.moveTo(x1, y1);
-    this.lineTo(x2, y2);
-    this.closePath();
-    this.stroke();
-  }
-  CanvasRenderingContext2D.prototype.circle = function(x, y, r, fill_opt) {
-    this.beginPath();
-    this.arc(x, y, r, 0, Math.PI * 2, true);
-    this.closePath();
-    if (fill_opt) {
-      this.fillStyle = 'rgba(0,0,0,1)';
-      this.fill();
-      this.stroke();
-    } else {
-      this.stroke();
-    }
-  }
-  CanvasRenderingContext2D.prototype.rectangle = function(x, y, w, h, fill_opt) {
-    this.beginPath();
-    this.rect(x, y, w, h);
-    this.closePath();
-    if (fill_opt) {
-      this.fillStyle = 'rgba(0,0,0,1)';
-      this.fill();
-    } else {
-      this.stroke();
-    }
-  }
-  CanvasRenderingContext2D.prototype.triangle = function(p1, p2, p3, fill_opt) {
-    // Stroked triangle.
-    this.beginPath();
-    this.moveTo(p1.x, p1.y);
-    this.lineTo(p2.x, p2.y);
-    this.lineTo(p3.x, p3.y);
-    this.closePath();
-    if (fill_opt) {
-      this.fillStyle = 'rgba(0,0,0,1)';
-      this.fill();
-    } else {
-      this.stroke();
-    }
-  }
-  CanvasRenderingContext2D.prototype.clear = function() {
-    this.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
-  }
-})();
 
 
 (function playButtonHandler() {
